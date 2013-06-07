4867d1e44330af4a2fa94741b8317168df3b4981
haobug <qingxianhao@gmail.com>
2013-06-08 01:40:10 +0800

    add ~~NEWPAGE~~ syntax for use this in wikipage

--- helper.php
+++ helper.php
@@ -33,9 +33,9 @@ class helper_plugin_npd extends DokuWiki_Plugin
 
         /* don't show the button if the user doesn't have edit permissions */
         if(auth_quickaclcheck($ID) < AUTH_EDIT) {
-			$non_login = $this->getConf('non_login_show');
-			if(!$non_login)
-				return '';
+            $non_login = $this->getConf('non_login_show');
+            if(!$non_login)
+                return '';
         }
 
         $label = $this->getLang('btn_create_new_page');
@@ -52,25 +52,29 @@ class helper_plugin_npd extends DokuWiki_Plugin
         $id = idfilter($ID,false);
 
 
-		if(!isset($_SERVER['REMOTE_USER'])) {
-			$act = tpl_get_action('login');
-			if(strpos($id, '#') === 0) {
-				$url = $id;
-			} else {
-				$url = wl($id, $act['params']);
-			}
+        if(!isset($_SERVER['REMOTE_USER'])) {
+            $act = tpl_get_action('login');
+            if(strpos($id, '#') === 0) {
+                $url = $id;
+            } else {
+                $url = wl($id, $act['params']);
+            }
 
-			$link_type = $this->getConf('link_type');
-			switch ($link_type) {
-			case 'link':
-				$out = tpl_link($url, $label, '', $return);
-				break;
-			default:
-				$out = html_btn('create_new', $id, false, $params, 'get','', $label);
-				break;
-			}
-			return $out;
-		} else {
+            $link_type = $this->getConf('link_type');
+            switch ($link_type) {
+            case 'link':
+                $out = tpl_link($url, $label, '', true);
+                break;
+            default:
+                $out = html_btn('create_new', $id, false, $params, 'get','', $label);
+                break;
+            }
+            if ($return) {
+                return $out;
+            } else {
+                echo $out;
+            }
+        } else {
         //make nice URLs even for buttons
         if($conf['userewrite'] == 2){
             $script = DOKU_BASE.DOKU_SCRIPT.'/'.$id . "?";
@@ -92,7 +96,7 @@ class helper_plugin_npd extends DokuWiki_Plugin
                 $url .= htmlspecialchars($val.'&');
             }
         }
-		}
+        }
         $link_type = $this->getConf('link_type');
 
         switch ($link_type) {
---
+++ syntax.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Keyboard Syntax Plugin: Marks text as keyboard key presses.
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Gina Haeussge <osd@foosel.net>
+ * @author     Christopher Arndt
+ */
+
+if(!defined('DOKU_INC'))
+  define('DOKU_INC',realpath(dirname(__FILE__).'/../../').'/');
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once(DOKU_PLUGIN.'syntax.php');
+
+/**
+ * All DokuWiki plugins to extend the parser/rendering mechanism
+ * need to inherit from this class
+ */
+class syntax_plugin_npd extends DokuWiki_Syntax_Plugin {
+
+    /**
+     * return some info
+     */
+    function getInfo() {
+        return confToHash(dirname(__FILE__).'/plugin.info.txt');
+    }
+
+    function getType() { return 'formatting'; }
+
+    function getSort(){ return 135; }
+
+    function connectTo($mode) {
+         $this->Lexer->addEntryPattern('~~NEWPAGE', $mode, 'plugin_npd');
+    }
+
+    function postConnect() {
+        $this->Lexer->addExitPattern('~~', 'plugin_npd');
+    }
+
+    /**
+     * Handle the match
+     */
+    function handle($match, $state, $pos, &$handler) {
+        switch ($state) {
+            case DOKU_LEXER_ENTER :
+            case DOKU_LEXER_UNMATCHED :
+            case DOKU_LEXER_EXIT:
+                return array($state, '');
+        }
+        return array();
+    }
+
+    /**
+     * Create output
+     */
+    function render($mode, &$renderer, $data) {
+        if ($mode == 'xhtml') {
+            list($state, $matches) = $data;
+            switch ($state) {
+                case DOKU_LEXER_ENTER :
+                    if(!($helper = plugin_load('helper', 'npd'))) return false;
+                    $renderer->doc .= $helper->html_new_page_button(true);
+                    return true;
+                case DOKU_LEXER_UNMATCHED :
+                case DOKU_LEXER_EXIT:
+                    return true;
+            }
+        }
+        return false;
+    }
+}
